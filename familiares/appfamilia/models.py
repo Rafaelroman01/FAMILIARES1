6e0788a3fd7915e7ff6cff5093b1947a81d34b2d
Rafael Roman <rafaelroman01@gmail.com>
2023-09-27 17:20:57 -0300

    Creacion de la appfamilia

---
+++ familiares/appfamilia/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Familia(models.Model):
+    nombre = models.CharField(max_length=50)
+    camada = models.IntegerField()
+    
+class Hijo(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+
+
+class Padre(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=50)
+    
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=50)
+    FechaDeEntrega = models.DateField()
+    email = models.EmailField()
+    entregado = models.BooleanField()
+    
