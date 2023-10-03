7312b65f5d8a7d2a867a267eb53e8fbb2275a475
Rafael Roman <rafaelroman01@gmail.com>
2023-10-03 06:46:23 -0300

    Creacion del panel de Administracion

--- familiares/appfamilia/admin.py
+++ familiares/appfamilia/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from appfamilia.models import *
 
 # Register your models here.
+admin.site.register (Familia)
+admin.site.register (Hijo)
+admin.site.register (Representante)
+admin.site.register (Entregable)
--- familiares/appfamilia/urls.py
+++ familiares/appfamilia/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from appfamilia.views import *
 
 urlpatterns = [
-    path('inic/', inic),
-    path('familias/', familias),
-    path('hijos/', hijos),
-    path('representante/', representantes),
-    path('entregables/', entregables)
+    path('inic/', inic, name="coder-inic"),
+    path('familias/', familias, name="coder-familias"),
+    path('hijos/', hijos, name="coder-hijos"),
+    path('representante/', representantes, name="coder-representante"),
+    path('entregables/', entregables, name="coder-entregables" )
 ]
